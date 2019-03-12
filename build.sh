d4df552e372cb0909f4601d5ecd4ddd3ca55d6d8
dongmingchao <1657883517@qq.com>
2019-03-12 12:06:41 +0800

    travis test .2

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ cp plantform/android/* dist/
 rm -rf $workspace/www/dist
 cp -r dist $workspace/www/dist
 cd $workspace
-
+touch $androidHome/release-signing.properties
 echo 'key.store=qialanshan-todo-release.keystore' >> $androidHome/release-signing.properties
 echo "key.alias=${APP_ALIAS}" >> $androidHome/release-signing.properties
 echo "key.store.password=${APP_PASSWORD}" >> $androidHome/release-signing.properties
