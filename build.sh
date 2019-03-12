e326b1d905cf24865d74d0a0a4921f1d60718a9b
dongmingchao <1657883517@qq.com>
2019-03-12 12:21:52 +0800

    travis test .5

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ cp platforms/android/* dist/
 rm -rf $workspace/www/dist
 cp -r dist $workspace/www/dist
 cd $workspace
+npm i
 echo 'key.store=qialanshan-todo-release.keystore' >> $androidHome/release-signing.properties
 echo "key.alias=${APP_ALIAS}" >> $androidHome/release-signing.properties
 echo "key.store.password=${APP_PASSWORD}" >> $androidHome/release-signing.properties
